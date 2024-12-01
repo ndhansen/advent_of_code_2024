d056c9af2ac55f5fbb4066fe5b4a2016c778f7f4
Nicholas Hansen <ndhansen@leskat.net>
2024-12-01 12:53:09 +0000

    Day 1

--- src/aoc_2024/day_1.py
+++ src/aoc_2024/day_1.py
@@ -1,11 +1,32 @@
+from collections import Counter
 from typing import Any
 
 from aoc.puzzle import PuzzleInput
 
 
+def parse_input(puzzle: PuzzleInput) -> tuple[list[int], list[int]]:
+    left, right = [], []
+    for line in puzzle.lines:
+        a, b = line.split("   ", 1)
+        left.append(int(a))
+        right.append(int(b))
+    return left, right
+
+
 def part_1(puzzle: PuzzleInput) -> Any:
-    pass
+    left, right = parse_input(puzzle)
+    left = sorted(left)
+    right = sorted(right)
+    total = 0
+    for a, b in zip(left, right):
+        total += abs(a - b)
+    return total
 
 
 def part_2(puzzle: PuzzleInput) -> Any:
-    pass
+    left, right = parse_input(puzzle)
+    reference = Counter(right)
+    total = 0
+    for num in left:
+        total += num * reference.get(num, 0)
+    return total
