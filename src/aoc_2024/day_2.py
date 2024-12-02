2fee4572352b5543f7f6c818ebae60536641db9f
Nicholas Hansen <ndhansen@leskat.net>
2024-12-02 06:15:10 +0000

    Day 2, I woke up an hour late 😿

--- src/aoc_2024/day_2.py
+++ src/aoc_2024/day_2.py
@@ -3,9 +3,59 @@ from typing import Any
 from aoc.puzzle import PuzzleInput
 
 
+def parse_input(puzzle: PuzzleInput) -> list[list[int]]:
+    reports = []
+    for line in puzzle.lines:
+        items = line.split(" ")
+        reports.append([int(x) for x in items])
+    return reports
+
+
+def is_safe(report: list[int]) -> bool:
+    diffs = []
+    for i in range(len(report) - 1):
+        diffs.append(report[i] - report[i + 1])
+
+    all_increasing = True
+    for i in diffs:
+        if i < 1 or i > 3:
+            all_increasing = False
+    if all_increasing is True:
+        return True
+
+    all_decreasing = True
+    for i in diffs:
+        if i < -3 or i > -1:
+            all_decreasing = False
+    if all_decreasing is True:
+        return True
+    return False
+
+
 def part_1(puzzle: PuzzleInput) -> Any:
-    pass
+    reports = parse_input(puzzle)
+    total = 0
+    for report in reports:
+        total += is_safe(report)
+    return total
+
+
+def is_mostly_safe(report: list[int]) -> bool:
+    if is_safe(report):
+        return True
+
+    for i in range(len(report)):
+        without = report.copy()
+        del without[i]
+        if is_safe(without) is True:
+            return True
+    return False
 
 
 def part_2(puzzle: PuzzleInput) -> Any:
-    pass
+    reports = parse_input(puzzle)
+    total = 0
+    for report in reports:
+        print(is_mostly_safe(report))
+        total += is_mostly_safe(report)
+    return total
