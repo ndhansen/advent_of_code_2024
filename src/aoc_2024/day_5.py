eef1bd6d85846e45336fcc3a7ba005381f803406
Nicholas Hansen <ndhansen@leskat.net>
2024-12-07 18:02:24 +0000

    Day 5 faster

--- src/aoc_2024/day_5.py
+++ src/aoc_2024/day_5.py
@@ -1,23 +1,11 @@
 import contextlib
-from collections import defaultdict
 from typing import Any
 
 import networkx as nx
 from aoc.puzzle import PuzzleInput
 
 
-def get_graph(puzzle: PuzzleInput) -> dict[int, set[int]]:
-    graph = defaultdict(set)
-    for line in puzzle.lines:
-        if line.strip() == "":
-            break
-        left_str, right_str = line.split("|")
-        left, right = int(left_str), int(right_str)
-        graph[right].add(left)
-    return graph
-
-
-def get_netgraph(puzzle: PuzzleInput) -> nx.DiGraph:
+def get_graph(puzzle: PuzzleInput) -> nx.DiGraph:
     edges = []
     for line in puzzle.lines:
         if line.strip() == "":
@@ -29,44 +17,7 @@ def get_netgraph(puzzle: PuzzleInput) -> nx.DiGraph:
     return nx.DiGraph(edges)
 
 
-def remove_other_nodes(graph: nx.DiGraph, retained_nodes: set[int]) -> nx.DiGraph:
-    local_graph = graph.copy()
-    other_nodes = set(local_graph.nodes.keys()) - retained_nodes
-    for other_node in other_nodes:
-        for in_src, _ in local_graph.in_edges(other_node):
-            for _, out_dst in local_graph.out_edges(other_node):
-                local_graph.add_edge(in_src, out_dst)
-
-        local_graph.remove_node(other_node)
-
-    to_remove = []
-    for a, b in local_graph.edges():
-        if a == b:
-            to_remove.append(a)
-    for node in to_remove:
-        local_graph.remove_edge(node, node)
-
-    return local_graph
-
-
-def check_valid(graph: nx.DiGraph, page_sets: list[list[int]]) -> None:
-    for pages in page_sets:
-        smallgraph = remove_other_nodes(graph, set(pages))
-        with contextlib.suppress(nx.NetworkXNoCycle):
-            if nx.find_cycle(smallgraph):
-                import pudb
-
-                pudb.set_trace()
-                msg = "Cycle found, this might not work."
-                raise ValueError(msg)
-        path = list(nx.algorithms.topological_sort(smallgraph))
-        print(path)
-        nx.drawing.nx_agraph.write_dot(
-            smallgraph, path="src/aoc_2024/inputs/day_5/test.dot"
-        )
-
-
-def get_updates(puzzle: PuzzleInput) -> list[list[int]]:
+def get_pagesets(puzzle: PuzzleInput) -> list[list[int]]:
     rules = []
     rules_raw = puzzle.raw.split("\n\n", 1)[1]
     for rule in rules_raw.strip().split("\n"):
@@ -78,68 +29,53 @@ def get_updates(puzzle: PuzzleInput) -> list[list[int]]:
     return rules
 
 
-def process_updates(graph: dict[int, set[int]], rules: list[list[int]]) -> int:
+def remove_other_nodes(graph: nx.DiGraph, retained_nodes: set[int]) -> nx.DiGraph:
+    local_graph = graph.copy()
+    other_nodes = set(local_graph.nodes.keys()) - retained_nodes
+    for other_node in other_nodes:
+        local_graph.remove_node(other_node)
+
+    return local_graph
+
+
+def get_correct_order(graph: nx.DiGraph, pages: list[int]) -> list[int]:
+    smallgraph = remove_other_nodes(graph, set(pages))
+    with contextlib.suppress(nx.NetworkXNoCycle):
+        nx.algorithms.find_cycle(smallgraph)
+        msg = "Cycle found, this might not work."
+        raise ValueError(msg)
+    path = list(nx.algorithms.topological_sort(smallgraph))
+    return path
+
+
+def get_valid_medians(graph: nx.DiGraph, page_sets: list[list[int]]) -> int:
     total = 0
-    i = 0
-    for rule in rules:
-        result = process_update(graph, rule)
-        if result:
-            print(f"rule {i} passed, result: {result}")
-            total += result
-        i += 1
+    for pages in page_sets:
+        result = get_correct_order(graph, pages)
+        if result == pages:
+            total += result[len(result) // 2]
     return total
-
-
-def process_update(graph: dict[int, set[int]], rule: list[int]) -> int | None:
-    for current, priors in graph.items():
-        if current in rule:
-            current_idx = rule.index(current)
-            for prior in priors:
-                if prior in rule and rule.index(prior) > current_idx:
-                    return None
-    return rule[len(rule) // 2]
-
-
-def maybe_reorder(current: int, priors: set[int], rule: list[int]) -> list[int] | None:
-    if current in rule:
-        current_idx = rule.index(current)
-        for prior in priors:
-            if prior in rule and rule.index(prior) < current_idx:
-                rule = rule.copy()
-                rule.remove(current)
-                prior_idx = rule.index(prior)
-                rule.insert(prior_idx, current)
-                return rule
-    return None
-
-
-def process_updates_2(graph: dict[int, set[int]], rules: list[list[int]]) -> int:
-    total = 0
-    for rule in rules:
-        total += process_update_2(graph, rule)
-    return total
-
-
-def process_update_2(graph: dict[int, set[int]], rule: list[int]) -> int:
-    for current, priors in graph.items():
-        while (new_rule := maybe_reorder(current, priors, rule)) is not None:
-            rule = new_rule
-    return rule[len(rule) // 2]
 
 
 def part_1(puzzle: PuzzleInput) -> Any:
-    netgraph = get_netgraph(puzzle)
+    pagesets = get_pagesets(puzzle)
     graph = get_graph(puzzle)
-    rules = get_updates(puzzle)
-    check_valid(netgraph, rules)
-    return process_updates(graph, rules)
+    return get_valid_medians(graph, pagesets)
+
+
+def get_all_medians(graph: nx.DiGraph, page_sets: list[list[int]]) -> int:
+    total = 0
+    for pages in page_sets:
+        result = get_correct_order(graph, pages)
+        total += result[len(result) // 2]
+    return total
 
 
 def part_2(puzzle: PuzzleInput) -> Any:
+    pagesets = get_pagesets(puzzle)
+    wrong_pagesets = []
     graph = get_graph(puzzle)
-    rules = get_updates(puzzle)
-    # wrong_rules = []
-    # for rule in rules:
-    #     if process_update(graph, rule) is None:
-    #         wrong_rules.append(rule)
-    # return process_updates_2(graph, wrong_rules)
+    for pages in pagesets:
+        if get_correct_order(graph, pages) != pages:
+            wrong_pagesets.append(pages)
+    return get_all_medians(graph, wrong_pagesets)
