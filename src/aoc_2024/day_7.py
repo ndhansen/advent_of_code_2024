5f321eb812e1ff58d41ec421c0e26ceda01cc736
Nicholas Hansen <ndhansen@leskat.net>
2024-12-07 13:57:35 +0000

    Day 7 fast

--- src/aoc_2024/day_7.py
+++ src/aoc_2024/day_7.py
@@ -1,6 +1,3 @@
-import itertools
-from collections.abc import Callable
-from operator import add, mul
 from typing import Any
 
 from aoc.puzzle import PuzzleInput
@@ -17,32 +14,37 @@ def parse_input(puzzle: PuzzleInput) -> list[tuple[int, list[int]]]:
     return equations
 
 
-def concat(a: int, b: int) -> int:
-    return int(str(a) + str(b))
+def unjoin(a: int, b: int) -> int:
+    if not str(a).endswith(str(b)):
+        msg = f"The digits of {b} must occur in that order at the end of {a}"
+        raise ValueError(msg)
+    return a // 10 ** (len(str(b)))
 
 
-def insert_operators(
-    target: int, nums: list[int], functions: tuple[Callable[[int, int], int], ...]
-) -> bool:
-    for operations in itertools.product(functions, repeat=len(nums) - 1):
-        current = nums[0]
-        for idx, i in enumerate(nums[1:]):
-            current = operations[idx](current, i)
-            if current > target:
-                break
+def solve_recursive(target: int, nums: list[int], joins: bool = False) -> bool:
+    if target < 0:
+        return False
 
-        if current == target:
+    if len(nums) == 1:
+        if nums[0] == target:
             return True
-    return False
+        return False
+
+    possible_targets = [target - nums[-1]]
+
+    if joins is True and str(target).endswith(str(nums[-1])):
+        possible_targets.append(unjoin(target, nums[-1]))
+
+    if target % nums[-1] == 0:
+        possible_targets.append(target // nums[-1])
+
+    return any(solve_recursive(t, nums[:-1], joins=joins) for t in possible_targets)
 
 
-def count_possible(
-    equations: list[tuple[int, list[int]]],
-    functions: tuple[Callable[[int, int], int], ...],
-) -> int:
+def count_possible(equations: list[tuple[int, list[int]]], joins: bool = False) -> int:
     total = 0
     for target, nums in tqdm(equations):
-        if insert_operators(target, nums, functions) is True:
+        if solve_recursive(target, nums, joins=joins) is True:
             total += target
 
     return total
@@ -50,9 +52,9 @@ def count_possible(
 
 def part_1(puzzle: PuzzleInput) -> Any:
     equations = parse_input(puzzle)
-    return count_possible(equations, (mul, add))
+    return count_possible(equations, joins=False)
 
 
 def part_2(puzzle: PuzzleInput) -> Any:
     equations = parse_input(puzzle)
-    return count_possible(equations, (mul, concat, add))
+    return count_possible(equations, joins=True)
