b148c3ded2fa7a3d24b34543b3bf56660ab6906f
Nicholas Hansen <ndhansen@leskat.net>
2024-12-10 06:35:30 +0000

    Day 10 part 2

--- src/aoc_2024/day_10.py
+++ src/aoc_2024/day_10.py
@@ -8,28 +8,31 @@ from frozendict import frozendict
 from tqdm import tqdm
 
 
-def parse_puzzle(puzzle: PuzzleInput) -> tuple[dict[Coord, int], list[Coord]]:
+def parse_puzzle(
+    puzzle: PuzzleInput,
+) -> tuple[dict[Coord, int], list[Coord], list[Coord]]:
     topo = {}
     trailheads = []
+    ends = []
     for row, line in enumerate(puzzle.lines):
         for col, char in enumerate(line):
             current = Coord(row, col)
             if char == "0":
                 trailheads.append(current)
+            if char == "9":
+                ends.append(current)
             if char == ".":
                 continue
             topo[current] = int(char)
-    return topo, trailheads
+    return topo, trailheads, ends
 
 
 def count_ends(start: Coord, topo: frozendict[Coord, int]):
-    seen: dict[Coord, int] = defaultdict(int)
-    seen2 = set()
+    seen = set()
 
     @cache
     def num_trails(start: Coord, topo: frozendict[Coord, int]) -> int:
         if topo[start] == 9:
-            seen[start] = 1
             return 1
 
         total = 0
@@ -41,37 +44,69 @@ def count_ends(start: Coord, topo: frozendict[Coord, int]):
         ]:
             next = start + direction
             if next in seen:
-                total += seen[next]
                 continue
             if next not in topo:
                 continue
             if topo[start] - topo[next] == -1:
+                seen.add(next)
                 total += num_trails(next, topo)
 
-        seen[start] = total
         return total
 
     found = num_trails(start, topo)
-    print(f"{start}: found {seen[start]}")
-    import pudb
-
-    pudb.set_trace()
-    return seen[start]
+    return found
 
 
 def part_1(puzzle: PuzzleInput) -> Any:
-    topo, heads = parse_puzzle(puzzle)
+    topo, heads, _ = parse_puzzle(puzzle)
     topo_frozen = frozendict(topo)
     total = 0
-    # import pudb
-    #
-    # pudb.set_trace()
     for head in tqdm(heads):
         total += count_ends(head, topo_frozen)
-        # print(found)
+    return total
+
+
+def iterative_increment(topo: dict[Coord, int], ends: list[Coord]) -> dict[Coord, int]:
+    to_explore = set(ends)
+    moves: dict[Coord, int] = defaultdict(lambda: 0)
+    for nine in ends:
+        moves[nine] = 1
+    for i in range(9, -1, -1):
+        next_to_explore = set()
+        while to_explore:
+            pos = to_explore.pop()
+            val = topo[pos]
+            if val != i:
+                msg = "Something went wrong"
+                raise ValueError(msg)
+
+            paths = 0
+            for direction in [
+                Direction.NORTH,
+                Direction.SOUTH,
+                Direction.EAST,
+                Direction.WEST,
+            ]:
+                walk = pos + direction
+                if walk in topo:
+                    if topo[walk] == i + 1:
+                        paths += moves[walk]
+                    if topo[walk] == i - 1:
+                        next_to_explore.add(walk)
+            # add to current so 9's stay at 1
+            moves[pos] += paths
+        to_explore = next_to_explore
+    return moves
+
+
+def get_all_paths(starts: list[Coord], moves: dict[Coord, int]) -> int:
+    total = 0
+    for start in starts:
+        total += moves[start]
     return total
 
 
 def part_2(puzzle: PuzzleInput) -> Any:
-    # 3618 too high
-    pass
+    topo, heads, ends = parse_puzzle(puzzle)
+    moves = iterative_increment(topo, ends)
+    return get_all_paths(heads, moves)
