7819821d4bba7ed466ca903a1c6b986f37f9a660
Nicholas Hansen <ndhansen@leskat.net>
2024-12-13 05:54:38 +0000

    Day 13 cleanup

--- src/aoc_2024/day_13.py
+++ src/aoc_2024/day_13.py
@@ -13,7 +13,7 @@ class Machine(NamedTuple):
     target: Coord
 
 
-def parse(puzzle: PuzzleInput) -> list[Machine]:
+def parse(puzzle: PuzzleInput, extra_target: int = 0) -> list[Machine]:
     machines = []
     button_pattern = r".*X\+(\d+), Y\+(\d+).*"
     prize_pattern = r".*X=(\d+), Y=(\d+).*"
@@ -26,55 +26,10 @@ def parse(puzzle: PuzzleInput) -> list[Machine]:
             Machine(
                 a=Coord(int(b1[1]), int(b1[0])),
                 b=Coord(int(b2[1]), int(b2[0])),
-                target=Coord(int(p[1]), int(p[0])),
+                target=Coord(int(p[1]) + extra_target, int(p[0]) + extra_target),
             )
         )
     return machines
-
-
-def parse_2(puzzle: PuzzleInput) -> list[Machine]:
-    machines = parse(puzzle)
-    big_machines = []
-    for machine in machines:
-        big_machines.append(
-            Machine(
-                a=machine.a,
-                b=machine.b,
-                target=Coord(
-                    machine.target.row + 10000000000000,
-                    machine.target.col + 10000000000000,
-                ),
-            )
-        )
-    return big_machines
-
-
-def bruteforce(machines: list[Machine]) -> int:
-    total = 0
-    for machine in machines:
-        lowest_cost = None
-        for a_presses in range(1, 101):
-            a_result = Coord(machine.a.row * a_presses, machine.a.col * a_presses)
-            if a_result > machine.target:
-                break
-            for b_presses in range(1, 101):
-                b_result = Coord(machine.b.row * b_presses, machine.b.col * b_presses)
-                result = a_result + b_result
-                if result > machine.target:
-                    break
-                if result != machine.target:
-                    continue
-
-                cost = (3 * a_presses) + b_presses
-                if lowest_cost is None:
-                    lowest_cost = cost
-                elif cost < lowest_cost:
-                    lowest_cost = cost
-
-        if lowest_cost is not None:
-            total += lowest_cost
-
-    return total
 
 
 def equation_solve(machines: list[Machine]) -> int:
@@ -95,9 +50,9 @@ def equation_solve(machines: list[Machine]) -> int:
 
 def part_1(puzzle: PuzzleInput) -> Any:
     machines = parse(puzzle)
-    return bruteforce(machines)
+    return equation_solve(machines)
 
 
 def part_2(puzzle: PuzzleInput) -> Any:
-    machines = parse_2(puzzle)
+    machines = parse(puzzle, 10000000000000)
     return equation_solve(machines)
