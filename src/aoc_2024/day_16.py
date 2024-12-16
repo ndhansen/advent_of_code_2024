80141c0b3e593ced8896e3bf675dfde13cd8901d
Nicholas Hansen <ndhansen@leskat.net>
2024-12-16 20:35:29 +0000

    Day 16 cleanup

--- src/aoc_2024/day_16.py
+++ src/aoc_2024/day_16.py
@@ -1,70 +1,13 @@
-import heapq
 import math
 from collections import defaultdict
 from collections.abc import Iterator, Mapping
-from typing import Any, Protocol
+from typing import Any
 
+from aoc.a_star import Cost, Heuristic, Neighbors, a_star
 from aoc.datatypes import Coord, Direction
 from aoc.exceptions import UnsolveableError
 from aoc.puzzle import PuzzleInput
 from tqdm import tqdm
-
-
-class Heuristic[T](Protocol):
-    def __call__(self, current: T, goal: T) -> float: ...
-
-
-class Cost[T](Protocol):
-    def __call__(self, paths: Mapping[T, T], current: T, last: T) -> float: ...
-
-
-class Neighbors[T](Protocol):
-    def __call__(self, current: T, paths: Mapping[T, T]) -> Iterator[T]: ...
-
-
-def _reconstruct_path[T](paths: Mapping[T, T], start: T, goal: T) -> list[T]:
-    path = [goal]
-    current = goal
-    while current in paths:
-        path.insert(0, paths[current])
-        current = paths[current]
-        if current == start:
-            break
-    return path
-
-
-def a_star[T](
-    start: T,
-    goal: T,
-    heuristic: Heuristic[T],
-    cost_func: Cost[T],
-    next_func: Neighbors[T],
-) -> tuple[list[T], float]:
-    frontier: list[tuple[float, T]] = []
-    heapq.heappush(frontier, (heuristic(start, goal), start))
-    paths: dict[T, T] = {}
-    cheapest_path: dict[T, float] = defaultdict(lambda: float("inf"))
-    cheapest_path[start] = 0.0
-
-    while len(frontier) > 0:
-        _, current = heapq.heappop(frontier)
-        if current == goal:
-            path = _reconstruct_path(paths, start, goal)
-            return path, cheapest_path[current]
-
-        for neighbor in next_func(current, paths):
-            new_cost = cheapest_path[current] + cost_func(paths, neighbor, current)
-
-            if new_cost < cheapest_path[neighbor]:
-                paths[neighbor] = current
-                cheapest_path[neighbor] = new_cost
-                heapq.heappush(
-                    frontier,
-                    (new_cost + heuristic(neighbor, goal), neighbor),
-                )
-
-    msg = "Could not find a path."
-    raise UnsolveableError(msg)
 
 
 class ReindeerHeuristic(Heuristic[Coord]):
@@ -139,6 +82,10 @@ def part_1(puzzle: PuzzleInput) -> Any:
 
 
 def find_all_fastest_wrong(maze: set[Coord], start: Coord, end: Coord) -> int:
+    """
+    I left this in because it works on the full puzzle input but fails on the tests.
+    It's incorrect.
+    """
     rh = ReindeerHeuristic()
     rc = ReindeerCost()
     rn = ReindeerNeighbors(maze)
@@ -231,5 +178,5 @@ def part_2(puzzle: PuzzleInput) -> Any:
     rh = ReindeerHeuristic()
     rc = ReindeerCost()
     rn = ReindeerNeighbors(maze)
-    path, cost = a_star(start, end, rh, rc, rn)
+    _, cost = a_star(start, end, rh, rc, rn)
     return bfs(maze, start, end, int(cost))
