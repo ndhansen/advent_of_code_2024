fcd08ea13488714106f8542db2cfe10fbe361c57
Nicholas Hansen <ndhansen@leskat.net>
2024-12-23 10:58:21 +0100

    Day 23 cleanup

--- src/aoc_2024/day_23.py
+++ src/aoc_2024/day_23.py
@@ -25,46 +25,6 @@ def part_1(puzzle: PuzzleInput) -> Any:
     return len(lan)
 
 
-def max_clique_bruteforce(graph: dict[str, list[str]]) -> frozenset[str]:
-    """Brute force find a max clique"""
-    network = {k: set(v) for k, v in graph.items()}
-    edges = set()
-    for k, vs in graph.items():
-        for v in vs:
-            edges.add(frozenset((k, v)))
-
-    current_largest = frozenset()
-    frontier = edges.copy()
-    seen = edges.copy()
-    while frontier:
-        current = list(frontier.pop())
-        interconnection = network[current[0]].copy()
-
-        # If our node has fewer connections than the current max, exit early
-        if len(interconnection) + 1 < len(current_largest):
-            continue
-
-        # Get all mutual connections
-        for o in current[1:]:
-            interconnection &= network[o]
-
-        if len(interconnection) == 0:
-            continue
-
-        for o in interconnection:
-            temp = set(current)
-            temp.add(o)
-            to_explore = frozenset(temp)
-            if to_explore in seen:
-                continue
-            frontier.add(frozenset(to_explore))
-            seen.add(frozenset(to_explore))
-            if len(to_explore) > len(current_largest):
-                current_largest = to_explore
-
-    return current_largest
-
-
 def maximal_clique_polynomial(graph: dict[str, list[str]]) -> frozenset[str]:
     """Faster brute force maximal clique"""
     largest_mc = frozenset()
@@ -85,16 +45,4 @@ def maximal_clique_polynomial(graph: dict[str, list[str]]) -> frozenset[str]:
 def part_2(puzzle: PuzzleInput) -> Any:
     network = parse(puzzle)
 
-    # start = time.time()
-    res = ",".join(sorted(list(maximal_clique_polynomial(network))))
-    # end = time.time()
-    # total_time = end - start
-    # print(res)
-    # print("Time taken:", total_time)
-    # start = time.time()
-    # res = ",".join(sorted(list(max_clique_bruteforce(network))))
-    # end = time.time()
-    # total_time = end - start
-    # print(res)
-    # print("Time taken:", total_time)
-    return res
+    return ",".join(sorted(list(maximal_clique_polynomial(network))))
