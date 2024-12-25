92b25a4be4eb41cc0b49ea32c93bb91aacf78a62
Nicholas Hansen <ndhansen@leskat.net>
2024-12-25 07:05:54 +0100

    Day 25 cleaner

--- src/aoc_2024/day_25.py
+++ src/aoc_2024/day_25.py
@@ -1,43 +1,48 @@
+import itertools
 from typing import Any
 
 from aoc.puzzle import PuzzleInput
 
+type Key = list[int]
+type Lock = list[int]
 
-def parse(puzzle: PuzzleInput) -> tuple[list[list[int]], list[list[int]]]:
-    bs = []
+
+def parse(puzzle: PuzzleInput) -> tuple[list[Lock], list[Key]]:
+    schematics = []
     for block in puzzle.raw.split("\n\n"):
         lines = block.splitlines()
-        lines_as_list = list(map(list, lines))
-        rotated = list(map(list, zip(*lines_as_list)))
-        bs.append(rotated)
+        lines_as_list = [list(x) for x in lines]
+        rotated = [list(x) for x in zip(*lines_as_list)]
+        schematics.append(rotated)
 
     locks = []
     keys = []
-    for b in bs:
+    for schematic in schematics:
         fig = []
-        for line in b:
+        for line in schematic:
             fig.append(line.count("#") - 1)
-        if b[0][0] == ".":  # It's a key
+        if schematic[0][0] == ".":  # It's a key
             keys.append(fig)
         else:
             locks.append(fig)
     return locks, keys
 
 
+def possible_key_combos(locks: list[Lock], keys: list[Key]) -> int:
+    total = 0
+    for key, lock in itertools.product(keys, locks):
+        fits = True
+        for bite, pin in zip(key, lock):
+            if bite + pin > 5:
+                fits = False
+        if fits is True:
+            total += 1
+    return total
+
+
 def part_1(puzzle: PuzzleInput) -> Any:
     locks, keys = parse(puzzle)
-    total = 0
-    for key in keys:
-        for lock in locks:
-            combo = []
-            fits = True
-            for a, b in zip(key, lock):
-                if a + b > 5:
-                    fits = False
-                combo.append(a + b)
-            if fits is True:
-                total += 1
-    return total
+    return possible_key_combos(locks, keys)
 
 
 def part_2(puzzle: PuzzleInput) -> Any:
